761a7c54cd10d1754079e537c547646ca5294fa2
youngdo <ky2d@naver.com>
2019-01-22 16:12:18 +0900

    a

---
+++ js/ui.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var dd = document.querySelectorAll('.comboList-a');
+    var html = document.querySelector('html');
+    var menu = document.querySelectorAll('.menu li')[0].querySelector('a');
+    
+    console.log(menu);
+    
+
+});
